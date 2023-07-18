313a3d237f2ec0583fce622ba54409e4e9ccbf6f
nayonsoso <panda0329@naver.com>
2023-07-18 17:51:27 +0900

    230718

--- src/main/java/com/example/websample/config/WebConfig.java
+++ src/main/java/com/example/websample/config/WebConfig.java
@@ -4,20 +4,29 @@ package com.example.websample.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public FilterRegistrationBean loggingFilter(){ // 필터를 등록해주기 위한 빈
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<Filter>();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.addUrlPatterns("/hey");
+        filterRegistrationBean.addUrlPatterns("/*");
 
         return filterRegistrationBean;
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/*", "/images/*");
+    }
 }
